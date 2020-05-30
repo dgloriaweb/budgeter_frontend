1ef7d2c32307bef1c8af9ed549af0c42c3c990b7
unknown <dgloria.web@gmail.com>
2020-05-30 18:43:20 +0100

    added bootstrap vue

---
+++ src/components/Main.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div>
+      <b-button size="sm" @click="toggle">
+        {{ show ? 'Hide' : 'Show' }} Alert
+      </b-button>
+      <b-alert v-model="show" class="mt-3" dismissible @dismissed="dismissed">
+        Hello {{ name }}!
+      </b-alert>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'BootstrapVue',
+      show: true,
+    }
+  },
+  watch: {
+    show(newVal) {
+      console.log('Alert is now ' + (newVal ? 'visible' : 'hidden'))
+    },
+  },
+  methods: {
+    toggle() {
+      console.log('Toggle button clicked')
+      this.show = !this.show
+    },
+    dismissed() {
+      console.log('Alert dismissed')
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
